08a3b1d8f19260aa4df0f4af5fe1f2a21ca294e7
Karthick <karthickk1996@hotmail.com>
2025-08-12 22:57:34 +0530

    Refactor table module by replacing inertiauiTable.js with inertiauiTable.ts, enhancing type safety and clarity. Clean up imports and streamline the codebase by removing the old JavaScript file.

--- modules/table/resources/js/inertiauiTable.js
+++
@@ -1,8 +0,0 @@
-import { getActionForItem, getClickableColumn, visitUrl } from './urlHelpers'
-import { setClauseSymbols } from './clauses'
-import { setIconResolver } from './iconResolver'
-import { useActions } from './actions'
-import { useTable } from './table'
-import Table from './TableComponent'
-
-export { getClickableColumn, setClauseSymbols, setIconResolver, Table, useActions, useTable, visitUrl, getActionForItem }
---
+++ modules/table/resources/js/inertiauiTable.ts
@@ -0,0 +1,8 @@
+import { useActions } from './actions';
+import { setClauseSymbols } from './clauses';
+import { setIconResolver } from './iconResolver';
+import { useTable } from './table';
+import Table from './TableComponent';
+import { getActionForItem, getClickableColumn, visitUrl } from './urlHelpers';
+
+export { getActionForItem, getClickableColumn, setClauseSymbols, setIconResolver, Table, useActions, useTable, visitUrl };
