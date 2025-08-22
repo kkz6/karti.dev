12937beb9e809293a267f7d0d5d7c62bb8e86e4e
Karthick <karthickk1996@hotmail.com>
2025-08-23 00:18:24 +0530

    Add migration for mediables table and create MediaAssetData DTO class; remove obsolete Media index page

---
+++ modules/media/database/migrations/2025_08_22_171010_create_mediables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Modules\Media\Models\Media;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mediables', function (Blueprint $table) {
+            $table->foreignIdFor(Media::class)->constrained('media')->cascadeOnDelete();
+            $table->morphs('mediable');
+            $table->string('tag')->index();
+            $table->unsignedInteger('order')->index();
+            $table->primary(['media_id', 'mediable_type', 'mediable_id', 'tag']);
+            $table->index(['mediable_id', 'mediable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mediables');
+    }
+};
