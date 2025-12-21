e9a13c36dd3e7f24cf9c072ede1eae81d5291f33
Karthick <karthickk1996@hotmail.com>
2025-12-21 20:27:35 +0530

    Revert OpenGraph meta tags - SSR is enabled

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,41 +30,13 @@
             html.dark {
                 background-color: rgb(24, 24, 27);
             }
-
+            
             body {
                 background-color: transparent;
             }
         </style>
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        {{-- OpenGraph Meta Tags (fallback for SSR) --}}
-        @if(isset($page['props']['seo']))
-            @php $seo = $page['props']['seo']; @endphp
-            @if(!empty($seo['title']))<meta property="og:title" content="{{ $seo['title'] }}">@endif
-            @if(!empty($seo['description']))<meta property="og:description" content="{{ $seo['description'] }}">@endif
-            @if(!empty($seo['image']))<meta property="og:image" content="{{ $seo['image'] }}">@endif
-            @if(!empty($seo['url']))<meta property="og:url" content="{{ $seo['url'] }}">@endif
-            @if(!empty($seo['type']))<meta property="og:type" content="{{ $seo['type'] }}">@endif
-            @if(!empty($seo['site_name']))<meta property="og:site_name" content="{{ $seo['site_name'] }}">@endif
-            @if(!empty($seo['locale']))<meta property="og:locale" content="{{ $seo['locale'] }}">@endif
-            @if(!empty($seo['description']))<meta name="description" content="{{ $seo['description'] }}">@endif
-            @if(!empty($seo['author']))<meta name="author" content="{{ $seo['author'] }}">@endif
-            {{-- Twitter Card --}}
-            @if(!empty($seo['twitter_card']))<meta name="twitter:card" content="{{ $seo['twitter_card'] }}">@endif
-            @if(!empty($seo['title']))<meta name="twitter:title" content="{{ $seo['title'] }}">@endif
-            @if(!empty($seo['description']))<meta name="twitter:description" content="{{ $seo['description'] }}">@endif
-            @if(!empty($seo['image']))<meta name="twitter:image" content="{{ $seo['image'] }}">@endif
-            @if(!empty($seo['twitter_site']))<meta name="twitter:site" content="{{ $seo['twitter_site'] }}">@endif
-            @if(!empty($seo['twitter_creator']))<meta name="twitter:creator" content="{{ $seo['twitter_creator'] }}">@endif
-            {{-- Canonical URL --}}
-            @if(!empty($seo['url']))<link rel="canonical" href="{{ $seo['url'] }}">@endif
-        @endif
-
-        {{-- JSON-LD Structured Data --}}
-        @if(isset($page['props']['jsonLd']))
-            <script type="application/ld+json">{!! json_encode($page['props']['jsonLd'], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}</script>
-        @endif
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
